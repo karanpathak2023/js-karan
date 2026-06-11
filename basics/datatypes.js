7964a65b32642761bac7ae6a4cfe24aa3dae45b5
Karan Pathak <pathakkaran393@gmail.com>
2026-06-11 10:46:41 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -3,4 +3,23 @@
 
 //alert(3+3) // will give error coz
              // we are using nodejs,not browser
-console.log("karan")             
+console.log("karan")
+let name= "karan" //string datatype
+let age= 21  //number
+let isLoggedIn = false  //boolean
+
+/*
+datatypes
+number => 2 to power 53
+bigint
+string
+boolean
+null => standalone value [ object h ]
+undefined =>
+symbol=> unique
+ 
+*/
+
+console.log(typeof "karan")
+console.log(typeof null ) //object result aayega 
+console.log(typeof undefined) //undefined aayega 
