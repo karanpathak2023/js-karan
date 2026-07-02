b90c87e7e0fcc400ddb14656e25b783b999b1eca
Karan Pathak <pathakkaran393@gmail.com>
2026-07-02 11:53:33 +0000

    rev

---
+++ basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use stricts"; //treat all js code as newer version
+//alert("hello")// we are using nodejs not browser
+
+console.log(log3+3)
+console.log("karan") //code readability should be high 
+
+let name = "karan"//string data type
+let age = 21 //num
+let isloggedin =  false // boolean
+//types of datatypes
+// number=> 2 to power 53
+//big int
+//string => ""
+//boolean => true/false
